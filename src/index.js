d6cd3e0700a7aaa362c00f3331e6a21c950f933b
Akshat Garg <coolakshat24@gmail.com>
2018-06-22 17:50:49 +0530

    Made application to search videos via Youtube API

--- src/index.js
+++ src/index.js
@@ -1,15 +1,48 @@
-import React from 'react';
+import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/SearchBar';
+import VideoList from './components/VideoList';
+import VideoDetail from './components/VideoDetail';
+import _ from 'lodash';
 
-import App from './components/app';
-import reducers from './reducers';
+const API_KEY = 'AIzaSyAdCWoFmz-UIioVjVV_ipNoPlHsNoaYCcI';
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
+class App extends Component {
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+	constructor(props) {
+		super(props);
+		this.state = {
+			selectedVideo: null,
+			videos : []
+		};
+	};
+
+	componentDidMount() {
+		this.videoSearch('surfboard');
+	}
+
+	videoSearch = (term) => {
+		YTSearch( {key :'AIzaSyAdCWoFmz-UIioVjVV_ipNoPlHsNoaYCcI', term : term }, (videos) => {
+			this.setState({
+				videos : videos,
+				selectedVideo: videos[0]
+			});
+		});
+	}
+
+	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+		return (
+			<div>
+				<SearchBar onSearchTermChange={videoSearch}/>
+				<VideoDetail video={this.state.selectedVideo} />
+				<VideoList
+					onVideoSelect={ selectedVideo => this.setState({selectedVideo})}
+					videos={this.state.videos} />
+			</div>
+		);
+	}
+}
+
+ReactDOM.render(<App />, document.getElementById('container'));
