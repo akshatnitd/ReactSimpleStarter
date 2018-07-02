04ab603d70bf95172d356ea33ff8215378a76903
akshatnitd <coolakshat24@gmail.com>
2018-07-02 18:29:26 +0530

    Add infinite scroll on button click

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import React , { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import SearchBar from './components/SearchBar';
+
 import VideoList from './components/VideoList';
 import VideoDetail from './components/VideoDetail';
+import axios from 'axios';
 import _ from 'lodash';
 
 const API_KEY = 'AIzaSyAdCWoFmz-UIioVjVV_ipNoPlHsNoaYCcI';
+const ROOT_URL = 'https://www.googleapis.com/youtube/v3/search';
 
 class App extends Component {
 
@@ -14,7 +16,9 @@ class App extends Component {
 		super(props);
 		this.state = {
 			selectedVideo: null,
-			videos : []
+			videos : [],
+            nextToken: '',
+            term: ''
 		};
 	};
 
@@ -23,23 +27,58 @@ class App extends Component {
 	}
 
 	videoSearch = (term) => {
-		YTSearch( {key :'AIzaSyAdCWoFmz-UIioVjVV_ipNoPlHsNoaYCcI', term : term }, (videos) => {
-			this.setState({
-				videos : videos,
-				selectedVideo: videos[0]
-			});
-		});
+
+        let params = {
+            part: 'snippet',
+            key: API_KEY,
+            q: term,
+            type: 'video'
+        };
+        if(this.state.nextToken) {
+            params.pageToken = this.state.nextToken;
+        } else if(params["pageToken"]) {
+            delete params.pageToken;
+        }
+
+        console.log(params);
+
+
+
+	   axios.get(ROOT_URL, { params: params })
+	    .then( (response) => {
+          this.setState(prevState =>({
+                videos : [...prevState.videos, ...response.data.items],
+                nextToken: response.data.nextPageToken
+            }));
+	    })
+        .then(() => {
+            this.setState({
+               selectedVideo: this.state.videos[0],
+            });
+        })
+	    .catch(function(error) {
+	      console.error(error);
+	    });
 	}
 
 	render() {
-		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+        const videoSearch = _.debounce((term) => {
+            this.setState({term});
+            this.videoSearch(term)
+        }, 300);
 		return (
 			<div>
-				<SearchBar onSearchTermChange={videoSearch}/>
+				<div className="search-bar">
+                <input
+                    value={this.state.term}
+                    onChange={event => videoSearch(event.target.value)}
+                    />
+                </div>
 				<VideoDetail video={this.state.selectedVideo} />
 				<VideoList
 					onVideoSelect={ selectedVideo => this.setState({selectedVideo})}
 					videos={this.state.videos} />
+                <button onClick={event => videoSearch(this.state.term)} className="btn btn-primary">More</button>
 			</div>
 		);
 	}
